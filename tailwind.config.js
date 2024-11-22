6e6c0d0a0cc6c838d30f8ca274bd9d4156dd2267
Sinclair <sinclairolajuwon@gmail.com>
2024-11-22 17:47:33 -0600

    starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        zentry:["zentry", "sanf-seriff"],
+        general: ["general", "sanf-seriff"],
+        "circular-web": ["circular-web", "sanf-seriff"],
+        "robert-regular": ["robert-regular", "sanf-seriff"],
+        "robert-medium": ["robert-medium", "sanf-seriff"]
+
+      }
+    },
   },
   plugins: [],
 }
