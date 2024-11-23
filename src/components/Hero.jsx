3656337a35d50896fbb442ef2a6a21ff257345c7
Sinclair <sinclairolajuwon@gmail.com>
2024-11-22 20:23:32 -0600

    starting to build the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -7,18 +7,35 @@ const Hero = () => {
     const [loadedVideos, setLoadedvideos] = useState(0);
     
     const totatVideo = 4;
-    const nextVdRef = useRef(null);
+    const nextVideoRef = useRef(null);
+
+    const getVideoSrc = (index) => `awwwards_public/public/videos/hero-${index}.mp4`
 
      const handleMiniVdClick = () => {
-
+        setHasClicked(true);
+        setCurrentIndex(prevIndex => prevIndex + 1);
      }
+
+     const handleVideoLoad = () => {
+        setLoadedvideos((prev)=> prev + 1)
+     }
+
+
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
         <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
             <div>
                 <div className='mask-clip-path absolute-center z-50 cursor-pointer rounded-lg overflow-hidden size-64'>
-                    <div>
-                        MiniVideoPlayer
+                    <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 cursor-pointer'>
+                        <video
+                            ref={nextVideoRef}
+                            src={getVideoSrc(currentIndex + 1)}
+                            loop
+                            muted
+                            id="current-video"
+                            className="size-64 origin-center scale-150 object-cover object-center"
+                            onLoadedData={handleVideoLoad}
+                        />
                     </div>
                 </div>
             </div>
