e4fe017354900bb9f5101e6a9aa50950216038fc
Sinclair <sinclairolajuwon@gmail.com>
2024-11-28 19:54:54 -0600

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
         <Hero/>
         <About/>
         <Features/>
+        <Story/>
+        <Contact />
+        <Footer />
       </main>
     </>
   )
